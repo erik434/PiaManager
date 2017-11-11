de85212885809a58f72069cd35501208d63ad843
Erik <erik434@users.noreply.github.com>
2017-11-11 12:04:05 -0700

    Added the option to stop or restart PIA
    Added some checks at startup to ensure we can find the PIA files

---
+++ PIA_manager/Extensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Management;
+
+namespace PIA_manager
+{
+    static class Extensions
+    {
+        //Method found here: https://stackoverflow.com/a/40501117
+        public static string GetCommandLine(this Process process)
+        {
+            string cmdLine = null;
+            using (var searcher = new ManagementObjectSearcher($"SELECT CommandLine FROM Win32_Process WHERE ProcessId = {process.Id}"))
+            {
+                //The query returns at most 1 match because the process is looked up by ID (which is unique by definition).
+                var matchEnum = searcher.Get().GetEnumerator();
+                if (matchEnum.MoveNext()) // Move to the 1st item.
+                {
+                    cmdLine = matchEnum.Current["CommandLine"]?.ToString();
+                }
+            }
+            //Commented out the below because we don't want to throw if this fails - returning null is fine.
+            //if (cmdLine == null)
+            //{
+            //    // Not having found a command line implies 1 of 2 exceptions, which the
+            //    // WMI query masked:
+            //    // An "Access denied" exception due to lack of privileges.
+            //    // A "Cannot process request because the process (<pid>) has exited."
+            //    // exception due to the process having terminated.
+            //    // We provoke the same exception again simply by accessing process.MainModule.
+            //    var dummy = process.MainModule; // Provoke exception.
+            //}
+            return cmdLine;
+        }
+
+        public static void KillTree(this Process process)
+        {
+            //Force kill the process by its PID and kill all of its children as well
+            var args = $"/F /PID {process.Id} /T";
+            var info = new ProcessStartInfo("taskkill", args)
+            {
+                UseShellExecute = false,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            using (var killer = Process.Start(info))
+            {
+                //TODO: add a timeout?
+                killer.WaitForExit();
+            }
+        }
+
+        public static bool Contains(this string s, string value, StringComparison comparisonType)
+        {
+            return s.IndexOf(value, comparisonType) > -1;
+        }
+    }
+}
--- PIA_manager/Program.cs
+++ PIA_manager/Program.cs
@@ -1,34 +1,121 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Diagnostics;
 
-namespace PIA
+/// <remarks>
+/// This project started out using code from Shukhrat Nekbaev: https://www.privateinternetaccess.com/forum/discussion/2286/pia-from-a-fixed-location-in-windows
+/// I've pretty much rewritten everything since then, so I think I'm clear to call this my own work at this point, but I'll leave the above as a reference.
+/// </remarks>
+namespace PIA_manager
 {
-    //Original author Shukhrat Nekbaev: https://www.privateinternetaccess.com/forum/discussion/2286/pia-from-a-fixed-location-in-windows
-    class PIA_Runner
+    /// <remarks>
+    /// Call this with no args to (re)start PIA - it will first kill an existing PIA process if one exists.
+    /// Call this with the argument "--stop" to just kill an existing PIA process.
+    /// </remarks>
+    static class Program
     {
+        const string PiaFilesDirName = "pia_ruby_files";
+
+        static string rubyExecutable;
+        static string rubySourceFile;
+        static string piaWorkingDir;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting...");
+            //First check that we can find the PIA directory and ruby files
+            FindPiaFiles();
 
-            var workingDir = Environment.CurrentDirectory;
-            var rubyFilesDir = Path.Combine(workingDir, "pia_ruby_files");
+            //Then run the desired command
+            if (args.Length > 0 && args[0] == "--stop")
+            {
+                StopPia();
+            }
+            else
+            {
+                StopPia();
+                StartPia();
+            }
+        }
 
-            Console.WriteLine("Working directory: {0}", workingDir);
-            Console.WriteLine("Ruby files directory: {0}", rubyFilesDir);
-
-            string rubyExecutable = Path.Combine(rubyFilesDir, "bin", "rubyw.exe");
-            string rubySourceFile = Path.Combine(rubyFilesDir, "src", "pia_manager.rb");
-            string rubyArgs = string.Format("\"{0}\" --run", rubySourceFile);
-
-            Console.WriteLine("Running: {0} {1}", rubyExecutable, rubyArgs);
+        private static void StartPia()
+        {
+            string rubyArgs = $"\"{rubySourceFile}\" --run";
+            Console.WriteLine($"Running: {rubyExecutable} {rubyArgs}");
 
             var info = new ProcessStartInfo(rubyExecutable, rubyArgs)
             {
-                WorkingDirectory = workingDir
+                WorkingDirectory = piaWorkingDir
             };
 
             Process.Start(info);
+        }
+
+        private static void StopPia()
+        {
+            //Kill the PIA processes
+            foreach (var process in Process.GetProcessesByName("rubyw"))
+            {
+                var cmdLine = process.GetCommandLine();
+                if (cmdLine == null)
+                {
+                    Console.WriteLine($"Found ruby process with PID {process.Id} but couldn't retrieve command line");
+                    continue;
+                }
+
+                Console.WriteLine($"Found Ruby process with PID: {process.Id}; cmd: {cmdLine}");
+
+                if (!cmdLine.Contains(rubySourceFile, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Does not look like a ruby process running PIA - skipping");
+                    continue;
+                }
+
+                //If we get down here, we found a rubyw process that is probably running the PIA manager code. It should be safe to kill it and its children.
+                process.KillTree();
+            }
+
+            //Delete the .pid files - may be important for PIA to restart properly later?
+            var logDir = Path.Combine(piaWorkingDir, "log");
+            if (!Directory.Exists(logDir))
+            {
+                Console.WriteLine("No log directory found, skipping pid cleanup");
+                return;
+            }
+
+            foreach(var pidFile in Directory.EnumerateFiles(logDir, "*.pid"))
+            {
+                Console.WriteLine($"Deleting pid file: {pidFile}");
+                File.Delete(pidFile);
+            }
+        }
+
+        private static void FindPiaFiles()
+        {
+            //Find the PIA files directory - first search where the .exe is running, and if that fails, try the current directory.
+            var exeDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var searchDirs = new[] { exeDir, Environment.CurrentDirectory };
+            piaWorkingDir = searchDirs.FirstOrDefault(DirContainsPiaFiles);
+            if (piaWorkingDir == null)
+            {
+                throw new DirectoryNotFoundException("Failed to find PIA ruby files directory");
+            }
+            var rubyFilesDir = Path.Combine(piaWorkingDir, PiaFilesDirName);
+
+            Console.WriteLine($"Working directory: {piaWorkingDir}");
+            Console.WriteLine($"Ruby files directory: {rubyFilesDir}");
+
+            rubyExecutable = Path.Combine(rubyFilesDir, "bin", "rubyw.exe");
+            rubySourceFile = Path.Combine(rubyFilesDir, "src", "pia_manager.rb");
+
+            if (!File.Exists(rubyExecutable)) throw new FileNotFoundException("Could not find ruby executable", rubyExecutable);
+            if (!File.Exists(rubySourceFile)) throw new FileNotFoundException("Could not find pia_manager ruby file", rubySourceFile);
+        }
+
+        static bool DirContainsPiaFiles(string searchDir)
+        {
+            return Directory.EnumerateDirectories(searchDir, PiaFilesDirName, SearchOption.TopDirectoryOnly).Any();
         }
     }
 }
